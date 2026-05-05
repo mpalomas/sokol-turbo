faab18fce90a41678cb2bc11911af044677c46cc
Michaël Palomas <mpalomas@proton.me>
2026-05-05 13:25:01 +0200

    added new bindgen impl c files

---
+++ bindgen/impl/sokol_color.c
@@ -0,0 +1,5 @@
+#if defined(IMPL)
+#define SOKOL_IMPL
+#endif
+#include "sokol_gfx.h"
+#include "sokol_color.h"
---
+++ bindgen/impl/sokol_fontstash.c
@@ -0,0 +1,12 @@
+#if defined(IMPL)
+#define FONTSTASH_IMPLEMENTATION
+#define SOKOL_IMPL
+#if defined(_MSC_VER)
+#pragma warning(disable:4996)
+#endif
+#include <stdlib.h>
+#include "fontstash.h"
+#endif
+#include "sokol_gfx.h"
+#include "sokol_gl.h"
+#include "sokol_fontstash.h"
